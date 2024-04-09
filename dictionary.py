318a07be334f01b9b258f5c450ed599174529ee6
EnverJ <Enlem1987@gmail.com>
2024-04-08 22:50:52 -0600

    added new chapters

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+MyDictionary = {"Peter": 30, "John": 50}
+print(MyDictionary)  # {'Peter': 30, 'John': 50}
+
+# use dic key word
+price = dict(Jony=38, Job=23)
+print(price)  # {'Jony': 38, 'Job': 23}
+
+price["Jony"] = 30;
+print(price)  # {'Jony': 30, 'Job': 23}
+
+# zero parameters dictioanry
+studentAge = {}
+studentAge["Josh"] = 40;
+studentAge["Tom"] = 32
+print(studentAge)  # {'Josh': 40, 'Tom': 32}
+
+print(studentAge["Josh"])  # 40
+
